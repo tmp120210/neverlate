42caf7da0cb568599541875707f4436ce9a25c9b
mikhail filippov <tmp120210@gmail.com>
2021-07-05 13:13:38 +0300

    initial commit with empty project

---
+++ Shared/NeverlateApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeverlateApp.swift
+//  Shared
+//
+//  Created by Mikhail Filippov on 05.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct NeverlateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
