7319f0b9c19c94bf88be69c4a9eb38dc71f8e2ff
CaptainTheAbysswalker <shuric03@gmail.com>
2021-07-05 17:43:53 +0300

    init first screen

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(){
+            VStack() {
+                Image("1")
+                    .aspectRatio(contentMode: .fill)
+                Spacer()
+                VStack(spacing: 16.0){
+                    Text("Welcome!")
+                        .font(.system(size: 22))
+                        .fontWeight(.heavy)
+                    Text("NeverLate is a perfect app for those who frequently participate in Zoom meetings")
+                        .font(.system(size: 18))
+                }
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Continue")
+                        .fontWeight(.regular)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, minHeight: 44)
+                        .background(Color("lightButton"))
+                })
+                .cornerRadius(10)
+                .buttonStyle(PlainButtonStyle())
+            }
+            .frame(width: 320, height: 540)
+            .padding(.horizontal, 16.0)
+            .padding(.vertical, 32.0)
+        }.background(Color.white)
     }
 }
 
--- Shared/NeverlateApp.swift
+++ Shared/NeverlateApp.swift
@@ -9,9 +9,44 @@ import SwiftUI
 
 @main
 struct NeverlateApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-            ContentView()
         }
     }
 }
+
+class AppDelegate: NSObject, NSApplicationDelegate{
+    
+    var StatusItem: NSStatusItem?
+    var popOver = NSPopover()
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        let menuView = ContentView()
+        
+        popOver.behavior = .transient
+        popOver.animates = true
+        popOver.contentViewController = NSViewController()
+        popOver.contentViewController?.view = NSHostingView(rootView: menuView)
+        
+        popOver.contentViewController?.view.window?.makeKey()
+        
+        StatusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let MenuButton = StatusItem?.button{
+            MenuButton.image = NSImage(systemSymbolName: "icloud.and.arrow.up.fill", accessibilityDescription: nil)
+            MenuButton.action = #selector(menuButtonToggle)
+        }
+        
+    }
+    @objc func menuButtonToggle(sender: AnyObject){
+        if popOver.isShown{
+            popOver.performClose(sender)
+        }else{
+            if let menuButton = StatusItem?.button{
+                self.popOver.show(relativeTo: menuButton.bounds, of: menuButton, preferredEdge: NSRectEdge.minY)
+            }
+        }
+    }
+}
+
