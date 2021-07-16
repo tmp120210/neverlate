ae124b60407104861e74c31325f5e298adeb0467
CaptainTheAbysswalker <shuric03@gmail.com>
2021-07-16 16:16:31 +0300

    try fix path

--- AutoLauncher/AppDelegate.swift
+++ AutoLauncher/AppDelegate.swift
@@ -12,11 +12,18 @@ class AutoLauncherAppDelegate: NSObject, NSApplicationDelegate {
     struct Constants {
         static let mainAppBundleID = "com.redrazzr.Neverlate"
         static let mainAppName = "Neverlate.app"
+        static let appTargetPlatform = "MacOS"
     }
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        print("launched")
+        
+        let killAutoLauncherNotificationName = Notification.Name(rawValue: "killAutoLauncher")
+                    DistributedNotificationCenter.default().addObserver(self,
+                                                                        selector: #selector(self.terminateApp),
+                                                                        name: killAutoLauncherNotificationName,
+                                                                        object: Constants.mainAppBundleID)
+        
         let runningApps = NSWorkspace.shared.runningApplications
         let isRunning = runningApps.contains{
             $0.bundleIdentifier == Constants.mainAppBundleID
@@ -28,12 +35,19 @@ class AutoLauncherAppDelegate: NSObject, NSApplicationDelegate {
             for _ in 1...3{
                 components.removeLast()
             }
+            components.append(Constants.appTargetPlatform)
             components.append(Constants.mainAppName)
+            
             let applicationPathString = NSString.path(withComponents: components)
             guard let pathURL = URL(string: "file://\(applicationPathString)") else {return}
             NSWorkspace.shared.openApplication(at: pathURL, configuration: NSWorkspace.OpenConfiguration(), completionHandler: nil)
         }
         
+        
     }
+    @objc
+        private func terminateApp() {
+            NSApp.terminate(nil)
+        }
 }
 
