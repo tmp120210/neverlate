d4e8b7a1e214ccec53793406cb8d1b4c22ef21a9
CaptainTheAbysswalker <shuric03@gmail.com>
2021-08-02 16:57:47 +0300

    menu icon added

--- Shared/NeverlateApp.swift
+++ Shared/NeverlateApp.swift
@@ -38,7 +38,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             withLength: CGFloat(NSStatusItem.variableLength))
         
         if let MenuButton = StatusItem?.button{
-            MenuButton.image = NSImage(named: "meeting_icon")
+            MenuButton.image = NSImage(named: "menuButton")
             MenuButton.action = #selector(menuButtonToggle)
             if startScreen != "meeting"{
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
